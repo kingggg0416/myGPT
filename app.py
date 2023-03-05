edca644d8efcb72e11c87c9d87eabfd3fa78d810
kingggg0416 <kelvin16042002@gmail.com>
2023-03-05 21:01:17 +0800

    add chat history feedback

--- app.py
+++ app.py
@@ -27,20 +27,21 @@ if 'chat_history' not in st.session_state:
         "avatar_style":"big-smile",
         "seed":"Aneka",
         "is_user": False}]
-
+    
 
 def generate_dialogue():
     user_msg = st.session_state.text_input
-    completion = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=[
-            {"role": "user", "content": user_msg}
-        ]
-    )
-    response_msg = completion.choices[0].message.content
     st.session_state.chat_history.append(
         {"message":user_msg, "is_user": True,"avatar_style":"adventurer", "key":str(uuid.uuid4())}
     )
+    completion = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role":"user" if chat["is_user"] else "system","content": chat["message"]} for chat in st.session_state.chat_history
+        ]
+    )
+    response_msg = completion.choices[0].message.content
+
     st.session_state.chat_history.append(
         {"message":response_msg, "is_user": False,"avatar_style":"big-smile","seed":"Aneka","key":str(uuid.uuid4())}
     )
