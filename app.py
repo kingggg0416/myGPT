e56f45dc3111d49b16fe362f20e741acfa925970
kingggg0416 <kelvin16042002@gmail.com>
2023-03-05 17:55:22 +0800

    modify app.py

--- app.py
+++ app.py
@@ -5,6 +5,16 @@ import uuid
 from streamlit_chat import message
 import streamlit_chat
 import openai
+st.set_page_config(
+    page_title="myGPT",
+    page_icon=":sparkles:",
+    initial_sidebar_state="expanded"
+)
+
+st.markdown("Made with love by Kelvin Wong. :blue_heart:")
+st.markdown(" - Follow me at Instagram @wongkingwang")
+st.markdown(" - Find me @<a href = 'https://www.linkedin.com/in/kelvinwonghkust/'>linkedln</a>",unsafe_allow_html=True)
+st.markdown("<hr>",unsafe_allow_html=True)
 
 openai.api_key = st.secrets["OPENAI_API_KEY"]
 
@@ -37,4 +47,19 @@ def generate_dialogue():
 for chat in st.session_state.chat_history:
     message(**chat)
 
-text_input = st.text_input("Your Question", key ='text_input', on_change=generate_dialogue)
+text_input = st.text_input("Your Question", key ='text_input',placeholder="Type something", on_change=generate_dialogue)
+
+
+
+# Side bar session
+with st.sidebar:
+    st.title("Welcome to myGPT!:balloon:")
+    st.markdown("What is myGPT?")
+    st.markdown('''
+    myGPT is a project created by me,
+    aiming to create an interesting chatting experience
+    for everyone, like you.''')
+    st.markdown("myGPT is built upon gpt-3.5-turbo, the lastest language model developed by OpenAI.")
+    st.write('''
+    If you want to know more about OpenAI, or chatGPT in general, click <a href = 'https://openai.com/'>here</a>
+    ''',unsafe_allow_html=True)
